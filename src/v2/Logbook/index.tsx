bba816cbaca0727045ae91bdab4d936384a9825e
tyler <tyler@cruftbusters.com>
2025-07-06 06:17:18 +0000

    add stub for logbook

---
+++ src/v2/Logbook/index.tsx
@@ -0,0 +1,14 @@
+export function Logbook() {
+  return (
+    <div>
+      <h1>Logbook</h1>
+      <p>
+        Logbook is for ad-hoc summarizations of transactions. Transactions could
+        be financial. However Logbook supports units beyond money like hours or
+        kilowatt hours or physical assets. Logbook is inspired by Datadog
+        notebooks and rich ad-hoc reporting and monitoring for software
+        telemetry.
+      </p>
+    </div>
+  )
+}
--- src/v2/index.tsx
+++ src/v2/index.tsx
@@ -1,19 +1,36 @@
+import { Route, Routes } from 'react-router-dom'
+import { Logbook } from './Logbook'
+
 export default function Index() {
   return (
-    <div>
-      <h1>Cruftbusters</h1>
-      <p>Cruftbusters is a flywheel for software.</p>
-      <h2>Principal</h2>
-      <div className="block">
-        <div>Tyler Johnson</div>
-        <div>Bothell, Washington, USA</div>
-        <div>
-          <a href="mailto:tyler@cruftbusters.com">tyler@cruftbusters.com</a>
-        </div>
-        <div>
-          <a href="tel:+16123562513">(612) 356-2513</a>
-        </div>
-      </div>
-    </div>
+    <Routes>
+      <Route
+        index
+        element={
+          <div>
+            <h1>Cruftbusters</h1>
+            <p>Cruftbusters is a flywheel for software.</p>
+            <h2>Principal</h2>
+            <div className="block">
+              <div>Tyler Johnson</div>
+              <div>Bothell, Washington, USA</div>
+              <div>
+                <a href="mailto:tyler@cruftbusters.com">
+                  tyler@cruftbusters.com
+                </a>
+              </div>
+              <div>
+                <a href="tel:+16123562513">(612) 356-2513</a>
+              </div>
+            </div>
+            <h2>Links</h2>
+            <p>
+              <a href="/apps/logbook">Logbook</a>
+            </p>
+          </div>
+        }
+      />
+      <Route path={'/apps/logbook'} element={<Logbook />} />
+    </Routes>
   )
 }
